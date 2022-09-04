da0efbb282f9496e006697ad39e86784afd83860
Facundo Iturrarte <facu.iturrarte@hotmail.com>
2022-09-04 19:41:03 -0300

    Revert "Merge branch 'main' of https://github.com/facundoiturrarte/facundoiturrarte.github.io"
    This reverts commit bd7d23a49e874b01f4abf557cc54032c904c4269, reversing
    changes made to 3fd31ab745f1ee457c71400926a8b60b6c05e3ff.

--- js/login.js
+++ js/login.js
@@ -4,12 +4,8 @@ let form = document.getElementById("Ingresar");
 let info = document.getElementById("info");
 
 function login() {
-<<<<<<< HEAD
     if (email.value.length == 0 || password.value.length >= 8) {
         localStorage.setItem("user",email.value);
-=======
-    if (email.value.length != 0 && password.value.length >= 8) {
->>>>>>> df563c4c725f17d1f1afe6cf7f48de5c4819fc29
         window.location.href = "index.html";
     } else {
         info.innerHTML = "La contraseña debe contener más de 8 caracteres"
